e63a39121769d3c0c14e9c749290e3dff0fed27b
dustinhaarmeyer <dustin05.dh@gmail.com>
2021-06-28 15:19:48 +0200

    Neu erstellen - Aufbau

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,43 @@ function App() {
   return (
     <body>
       <div className="App">
-      <header className="App-header">
-        <p>
-          My first Website
-        </p>
-        <h1>
-          Test
-        </h1>
-      </header>
-    </div>
-    </body>
+        <div className="titel">
+          <h1>ToDo - Liste</h1>
+        </div>
+        <div>
+          <div className="split liste">
+            <p>Todos</p>
+          </div>
+        </div>
+        <div>
+          <div className="split eingabe">
+            <h4>Neu erstellen</h4> <br></br>
+            <div className="Parameter">
+              <label htmlFor="fname">Name:</label><br></br>
+              <input className="input" type="text" id="fname" name="firstname" placeholder="Aufgabe"></input>
+              <br></br><br></br>
+              <label htmlFor="lname">Gruppe:</label><br></br>
+              <input className="input" type="text" id="lname" name="lastname" placeholder="Standard"></input>
+              <br></br><br></br>
+              <label htmlFor="lname">Datum</label><br></br>
+              <input className="input" type="text" id="lname" name="lastname" placeholder="DD/MM"></input>
+              <br></br><br></br>
+              <label htmlFor="country">Priorität:</label><br></br>
+              <select className="submit" id="country" name="Prio">
+                <option value="0">Geringe Priorität</option>
+                <option value="1">Normale Priorität</option>
+                <option value="2">Hohe Priorität</option>
+                <option value="3">Sehr hohe Priorität</option>
+              </select>
+              <br></br><br></br>
+            </div>
+            <div>
+              <input className="submit" type="submit" value="Erstellen"></input>
+            </div>
+          </div>
+        </div>
+      </div>
+    </body >
   );
 }
 
